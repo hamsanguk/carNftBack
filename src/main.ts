2090ef502ec51f44299759b686369c5776c429f8
hamsanguk <gtd5309@gmail.com>
2025-06-29 17:56:18 +0900

    save DB no error without mint

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import {ValidationPipe} from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port  = process.env.PORT;
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted:true,
     transform:true,
-  }))
-  await app.listen(process.env.PORT ?? 3000);
+  }));
+
+  await app.listen(port ?? 3000);
+  console.log(`Server running on http://localhost:${port}`)
 }
 bootstrap();
